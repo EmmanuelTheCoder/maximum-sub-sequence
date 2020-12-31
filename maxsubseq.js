b06b5ba3cac4ef3f47292bd25a7914c9ca526460
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2020-12-31 19:41:29 +0100

    solution to puzzle

---
+++ maxsubseq.js
@@ -0,0 +1,27 @@
+//The codes solve the maximum sum sequence puzzle!
+
+
+readline();
+const a = readline().split(' ').map(Number);
+
+let incArr = [];
+let max = 0;
+
+a.forEach((start, index) => {
+    let a0 = start;
+    const subArray = a.filter((an, i) => {
+        if (i < index) return false;
+        else if (an === a0) {
+            a0 += 1;
+            return true;
+        }
+        return false;
+    });
+    if (max === subArray.length && incArr[0] > subArray[0] ||
+        max < subArray.length) {
+        incArr = subArray;
+        max = incArr.length;
+    }
+});
+
+console.log(incArr.join(' '));
